17944cf6f329a658510d78b9c5129557bc293b8b
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2024-02-19 18:58:44 -0800

    Added Leaderboard

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,7 +1,22 @@
-import React from "react";
+import { useState } from "react";
 import "./Result.scss";
 
 const Result = ({ totalQuestions, result, onTryAgain }) => {
+  const [name, setName] = useState("");
+  const [highScores, setHighScores] = useState([]);
+  const [showScores, setShowScores] = useState(false);
+
+  const handleSave = () => {
+    const score = {
+      name,
+      score: result.score,
+    };
+
+    const newHighScores = [...highScores, score].sort((a, b) => b.score - a.score);
+    setHighScores(newHighScores);
+    setShowScores(true);
+  };
+
   return (
     <div className="result">
       <h3>Results</h3>
@@ -18,6 +33,36 @@ const Result = ({ totalQuestions, result, onTryAgain }) => {
         Total Score: <span>{result.score}</span>
       </p>
       <button onClick={onTryAgain}>Try Again</button>
+      {!showScores ? (
+        <>
+          <h3>Add to Leaderboard</h3>
+          <input placeholder="Name" value={name} onChange={(evt) => setName(evt.target.value)} />
+          <button onClick={handleSave}>Save</button>
+        </>
+      ) : (
+        <>
+          <table>
+            <thead>
+              <tr>
+                <th>Ranking</th>
+                <th>Name</th>
+                <th>Score</th>
+              </tr>
+            </thead>
+            <tbody>
+              {highScores.map((highScore, i) => {
+                return (
+                  <tr>
+                    <td>{i + 1}</td>
+                    <td>{highScore.name}</td>
+                    <td>{highScore.score}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </>
+      )}
     </div>
   );
 };
