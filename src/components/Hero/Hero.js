231c34d15629498807928b09a51b6f1542bf1999
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2024-02-22 01:01:58 -0800

    Updated Hero Button Styling

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,7 +6,7 @@ import "./Hero.scss";
 export default function Hero() {
   return (
     <section className="hero">
-      <div className="container flex flex-row space-between align-items-flex-end medium-gap flex-wrap max-width">
+      <div className="container flex flex-row space-between align-items-flex-end medium-gap max-width">
         <h1 className="primary-heading">Are you up to the test?</h1>
         <HeroButton buttonLink="/exam" buttonText="Try Now"></HeroButton>
       </div>
